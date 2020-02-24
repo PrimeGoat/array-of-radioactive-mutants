a1e1725d34b344fac9450b1599192241ea7d9e8c
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-02-24 14:42:43 -0500

    Done.

--- main.js
+++ main.js
@@ -2,9 +2,37 @@
  * YOUR CODE HERE *
  ******************/
 
+function addMeToEnd(array) {
+  array.push("Colin");
+}
 
+function addMeToStart(array) {
+  array.unshift("Colin");
+}
 
+function changeLast(array, value) {
+  array[array.length-1] = value
+}
 
+function changeAllValuesTo(array, value) {
+  for(let i = 0; i < array.length; i++) {
+    array[i] = value;
+  }
+}
+
+function oddOrEven(array) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] % 2 === 0) {
+      array[i] = "even";
+    } else {
+      array[i] = "odd";
+    }
+  }
+}
+
+function changeNextThreeToValue(index, array, value) {
+  array.splice(index, 3, value, value, value);
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
